12ed48c455334bfb30531312be96f3eba8ee48e2
Luke Gardener <luke.g@bitocean.com>
2019-11-06 11:21:59 +0900

    Added additional test for enque, deque, enque

--- src/test/java/com/arkumbra/paypay/ImmutableFifoQueueTest.java
+++ src/test/java/com/arkumbra/paypay/ImmutableFifoQueueTest.java
@@ -76,18 +76,55 @@ public class ImmutableFifoQueueTest {
   }
 
   @Test
+  public void testConsecutiveEnqueDeques() {
+    String element1 = "1";
+    String element2 = "2";
+    String element3 = "3";
+    String element4 = "4";
+    String element5 = "5";
+    String element6 = "6";
+
+    Queue<String> queue = new ImmutableFifoQueue<String>();
+    queue = queue.enQueue(element1);
+    queue = queue.enQueue(element2);
+
+    queue = queue.enQueue(element3);
+    assertEquals(element1, queue.head());
+
+    queue = queue.deQueue();
+    assertEquals(element2, queue.head());
+
+    queue = queue.enQueue(element4);
+    assertEquals(element2, queue.head());
+    queue = queue.enQueue(element5);
+    assertEquals(element2, queue.head());
+    queue = queue.enQueue(element6);
+    assertEquals(element2, queue.head());
+
+    queue = queue.deQueue();
+    assertEquals(element3, queue.head());
+
+    queue = queue.deQueue();
+    assertEquals(element4, queue.head());
+
+    queue = queue.deQueue();
+    assertEquals(element5, queue.head());
+
+    queue = queue.deQueue();
+    assertEquals(element6, queue.head());
+
+    queue = queue.deQueue();
+    assertTrue(queue.isEmpty());
+  }
+
+  @Test
   public void testForkingAQueueCantInterfereWithEachother() {
     String base = "base";
     Queue<String> baseOfQueue = new ImmutableFifoQueue<String>();
     baseOfQueue = baseOfQueue.enQueue(base);
 
     String fork1Val1 = "1";
-    String fork1Val2 = "2";
-    String fork1Val3 = "3";
-
     String fork2Val1 = "9";
-    String fork2Val2 = "8";
-    String fork2Val3 = "7";
 
     Queue<String> fork1 = baseOfQueue.enQueue(fork1Val1);
     Queue<String> fork2 = baseOfQueue.enQueue(fork2Val1);
